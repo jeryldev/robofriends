58fb658889856a6b9f6b2f04ef8b167214199cf5
Jeryl Donato Estopace <jeryldev@gmail.com>
2020-03-19 22:19:21 +0800

    destructured the props inside the Card component

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import 'tachyons';
 
-const Card = props => {
+const Card = ({ id, name, email }) => {
+  // destructure
+  // const { id, name, email } = props;
   return (
-    <div className='bg-light-green dib br3 pa3 ma3 grow bw2 shadow-5'>
-      <img src='https://robohash.org/test?size=200x200' alt='robot image' />
+    <div className='tc bg-light-green dib br3 pa3 ma3 grow bw2 shadow-5'>
+      <img src={`https://robohash.org/${id}?size=200x200`} alt='robot image' />
       <div>
-        <h2>{props.name}</h2>
-      </div>
-      <div>
-        <h2>{props.email}</h2>
+        <h2>{name}</h2>
+        <h2>{email}</h2>
       </div>
     </div>
   );
