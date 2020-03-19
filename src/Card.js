1eb1a7d5b56a035813a69f1e1ae9332e0c3e8b78
Jeryl Donato Estopace <jeryldev@gmail.com>
2020-03-19 21:29:18 +0800

    stylized the Card component

--- src/Card.js
+++ src/Card.js
@@ -3,8 +3,8 @@ import 'tachyons';
 
 const Card = props => {
   return (
-    <div>
-      <img src='' alt='robot image' />
+    <div className='bg-light-green dib br3 pa3 ma3 grow bw2 shadow-5'>
+      <img src='https://robohash.org/test?size=200x200' alt='robot image' />
       <div>
         <h2>Jeryl</h2>
       </div>
