626dc797e06e7427f29a413a59eddad98922c816
Jeryl Donato Estopace <jeryldev@gmail.com>
2020-03-19 20:33:12 +0800

    Deleted the Hello Stateless component and createed Card component

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import 'tachyons';
+
+const Card = props => {
+  return (
+    <div>
+      <img src='' alt='robot image' />
+      <div>
+        <h2>Jeryl</h2>
+      </div>
+      <div>
+        <h2>test@test.com</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
