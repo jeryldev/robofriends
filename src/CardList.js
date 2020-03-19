e50d8f35948668cc1d3f11c85fdf6114f4ce9774
Jeryl Donato Estopace <jeryldev@gmail.com>
2020-03-20 07:50:35 +0800

    refactored the CardsList component

--- src/CardList.js
+++ src/CardList.js
@@ -2,23 +2,20 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-  const cardsArray = robots.map(user => {
-    // console.log(user);
-    // console.log(robots);
-    return (
-      <Card
-        // id={robots[index].id}
-        // name={robots[index].name}
-        // email={robots[index].email}
-        key={user.id}
-        id={user.id}
-        name={user.name}
-        email={user.email}
-      />
-    );
-  });
-
-  return <div>{cardsArray}</div>;
+  return (
+    <div>
+      {robots.map(user => {
+        return (
+          <Card
+            key={user.id}
+            id={user.id}
+            name={user.name}
+            email={user.email}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default CardList;
