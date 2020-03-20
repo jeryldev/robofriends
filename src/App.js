187544878d0dc6c5bc4cf4c240e90af17edd8acd
Jeryl Donato Estopace <jeryldev@gmail.com>
2020-03-21 02:23:27 +0800

    Create Scroll Component that uses props.children

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardsList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
@@ -40,7 +41,9 @@ class App extends Component {
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
-          <CardsList robots={filteredRobots} />
+          <Scroll>
+            <CardsList robots={filteredRobots} />
+          </Scroll>
         </div>
       );
     }
