8e102e5c980fd5469b8414cc640b2bfac9e70d68
Jeryl Donato Estopace <jeryldev@gmail.com>
2020-03-27 20:42:53 +0800

    updated the mapStateToProps and render in App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,35 +6,41 @@ import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 
 const mapStateToProps = state => {
   return {
-    searchField: state.searchField
+    // searchField: state.searchField
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    onSearchChange: event => dispatch(setSearchField(event.target.value))
+    onSearchChange: event => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => requestRobots(dispatch)
   };
 };
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: []
-      // searchfield: ''
-    };
-  }
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     robots: []
+  //     // searchfield: ''
+  //   };
+  // }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => {
-        this.setState({ robots: users });
-      });
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    //   .then(response => response.json())
+    //   .then(users => {
+    //     this.setState({ robots: users });
+    //   });
+    this.props.onRequestRobots();
   }
 
   // onSearchChange = event => {
@@ -45,14 +51,16 @@ class App extends Component {
 
   render() {
     // const { robots, searchfield } = this.state;
-    const { robots } = this.state;
-    const { searchField, onSearchChange } = this.props;
+    // const { robots } = this.state;
+    const { searchField, onSearchChange, robots, isPending } = this.props;
+
     const filteredRobots = robots.filter(robot => {
       // return robot.name.toLowerCase().includes(searchfield.toLowerCase());
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
 
-    return robots.length === 0 ? (
+    // return robots.length === 0 ?
+    return isPending ? (
       <h1 className='tc'>Loading...</h1>
     ) : (
       <div className='tc'>
